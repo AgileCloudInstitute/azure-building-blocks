f8fdfa7bce45b586a6ef0f624c346a2d92057620
AgileCloudInstitute <Agile.Cloud.Institute@GreenRiverIT.com>
2022-12-30 14:53:28 -0800

    more upgrades

--- terraform/modules/emptyfoundation/resource-group.tf
+++
@@ -1,7 +0,0 @@
-#Copyright 2022 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.
-#Start at https://github.com/AgileCloudInstitute?tab=repositories
-
-resource "azurerm_resource_group" "pipelines" {
-  name     = var.resourceGroupName
-  location = var.resourceGroupRegion
-}
--- terraform/modules/foundation/networking-for-agents.tf
+++
@@ -1,9 +0,0 @@
-#Copyright 2021 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.
-#Start at https://github.com/AgileCloudInstitute?tab=repositories
-
-resource "azurerm_virtual_network" "pipelines" {
-  name                = "pipelineVirtnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.pipelines.location
-  resource_group_name = azurerm_resource_group.pipelines.name
-}
